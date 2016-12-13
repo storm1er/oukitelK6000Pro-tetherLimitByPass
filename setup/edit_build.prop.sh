9a9eeebab357934bafa268a5b0fd22ccd78747b1
Storm1er <le.storm1er@gmail.com>
2016-12-13 15:57:17 +0100

    initial commit with init.d enabler, build.prop editor, init.d script

---
+++ setup/edit_build.prop.sh
@@ -0,0 +1,66 @@
+#!/system/bin/sh
+# Build.prop editor script with basic sed commands
+# tommytomatoe
+# May 12, 2012
+# script from http://themikmik.com/showthread.php?12566-Scripts-Bash-and-Shell-scripts-for-updating-build-prop-in-recovery
+
+export PATH=/tmp:$PATH:/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin
+exec 0>&1
+
+set +e
+
+# mounting system as rw
+busybox mount -o remount,rw /system
+if [ $? != 0 ] ; then exit
+fi
+
+# Variable declarations
+# these are for the build.prop
+FILE=/system/build.prop
+TMPFILE=$FILE.tmp
+
+# PROP VARIABLE
+# these varaibles are the actual props inside the build prop we want to modify
+# I chose three popular ones to test on
+# this list can be as large as you want it to be
+line1=net.tethering.noprovisioning
+# ARGUMENT VARIABLE
+# these variables are the new values you want
+line1Arg=true
+
+# this variable is just a place holder for the future line number of the prop values
+lineNum=
+
+# main is where all the magic happens
+
+# to avoid clutter, I've moved some code comments to the bottom
+# each listing is matched with the correct marker #a will be #a below, etc
+
+#b = prop is set to argument 1, the prop variable
+#c = arg is set to argument 2, the argument variable
+#d = if prop value exists in build.prop,
+#    replace it, else, append it to end of file
+#e = lineNum will be set to the line number of prop
+#f = use sed to replace the line at lineNum with the new values
+#    alternatively, I could use this:
+        #sed -i "/${prop}/d" $FILE
+        #sed -i "${lineNum}i${prop}=${arg}" $FILE
+
+
+prop=$line1Arg										#b
+arg=$line1Arg										#c
+if grep -Fq $prop $FILE ; then						#d
+    lineNum=`sed -n "/${prop}/=" $FILE`				#e
+    echo $lineNum
+    sed -i "${lineNum} c${prop}=${arg}" $FILE		#f
+else
+    echo "$prop does not exist in build.prop"
+    echo "appending to end of build.prop"
+    echo $prop=$arg >> build.prop
+fi
+
+# to iterate over all the prop values you want to change,
+# just copy and paste the code chunk or create a for loop.
+# I will leave it to you to create a for loop
+
+exit 0
